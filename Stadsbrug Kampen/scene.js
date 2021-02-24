cb2684dbf2e1a96fca2fec5a4fc9214086dffe98
ridewo <ridewo@outlook.com>
2021-02-24 15:52:18 +0100

    fixed key open

--- Stadsbrug Kampen/scene.js
+++ Stadsbrug Kampen/scene.js
@@ -55,12 +55,12 @@ gui.open();
         // o = open, c = close
         if (e.code == "KeyO") //o
         {
-            open = true;
+            params["open"] = true;
             //open
         }
         if (e.code == "KeyC") //c
         {
-            open = false;
+            params["open"] = false;
             //close
         }
 
